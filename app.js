fa5d893ba08fbabcad65df5a1d99576e792792cf
Nikhil Sachdeva <nikhilsachdeva57@gmail.com>
2019-04-19 22:17:21 +0530

    init

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+
+const mysql = require('mysql');
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended:false})
+)
+app.use(express.static('./front'))
+
+const connection = mysql.createConnection({
+		host     : 'localhost',
+		user     : 'root',
+		password : 'Nikhil99',
+		database : 'MUSIC'
+})
+
+
+var server = app.listen(process.env.PORT || 8080, function () {
+   var port = server.address().port;
+   console.log("App now running on port", port);
+});
+
+app.get('/songs',(req,res) => {
+	connection.query("SELECT * FROM songs",(err,rows,query) =>{
+		res.json(rows)
+	})
+})
+
+// created using create table songs ( SongID int AUTO_INCREMENT, SongName varchar(30), Genre varchar(30), Duration int, Artist varchar(30), PRIMARY KEY (SongID));
---
+++ front/scripts.js
@@ -0,0 +1,33 @@
+
+
+var request = new XMLHttpRequest()
+var i=1;
+request.open('GET', 'http://localhost:8080/songs', true)
+request.onload = function() {
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response)
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(song => {
+      
+      var table = document.getElementById("songTable");
+      var row = table.insertRow(i);
+      var cell1 = row.insertCell(0);
+      var cell2 = row.insertCell(1);
+      var cell3 = row.insertCell(2);
+      var cell4 = row.insertCell(3);
+      var cell5 = row.insertCell(4);
+      cell1.innerHTML = song.SongID;
+      cell2.innerHTML = song.SongName;
+      cell3.innerHTML = song.Genre;
+      cell4.innerHTML = song.Duration/60;
+      cell5.innerHTML = song.Artist;
+      i=i+1;
+  
+    })
+  } else {
+    const errorMessage = document.createElement('marquee')
+    errorMessage.textContent = `Not working`
+    app.appendChild(errorMessage)
+  }
+}
+request.send()
